7705a858b365f751fea02ce7b404cfc13ad260a9
Laurent Navarro <ln@altidev.com>
2016-12-28 08:07:03 +0100

    Added splitf,splitc,join implementation and a formal proposal

--- testcpp17_1/testcpp17_1.cpp
+++ testcpp17_1/testcpp17_1.cpp
@@ -45,6 +45,34 @@ public:
 		return tmp.splitsv(Separator);  
 	}
 
+	template<class T,class U>
+	static basic_string<CharT, Traits, Allocator> join(T &InputStringList, U Separator)
+	{
+		basic_string<CharT, Traits, Allocator> result_string;
+		for (auto it = InputStringList.begin(); it != InputStringList.end(); it++)
+		{
+			if (it != InputStringList.begin())
+				result_string += Separator;
+			result_string += *it;
+
+		}
+		return result_string;
+	}
+	template<class T>
+	basic_string<CharT, Traits, Allocator> join(T &InputStringList)
+	{
+		basic_string<CharT, Traits, Allocator> result_string;
+		for (auto it = InputStringList.begin(); it != InputStringList.end(); it++)
+		{
+			if (it != InputStringList.begin())
+				result_string += *this;
+			result_string += *it;
+
+		}
+		return result_string;
+	}
+
+
 };
 typedef basic_string17<char> string17;
 
@@ -58,7 +86,7 @@ class basic_string_view17 :public basic_string_view<CharT, Traits>
 public:
 	using basic_string_view<CharT, Traits>::basic_string_view;
 
-	vector<basic_string<CharT, Traits> > splits(const  basic_string_view<CharT, Traits> &Separator) const
+	vector<basic_string<CharT, Traits> > splits(const basic_string_view<CharT, Traits> &Separator) const
 	{
 		vector<basic_string<CharT, Traits> > Result;
 		typename basic_string_view<CharT, Traits>::size_type p, start = 0;
@@ -78,7 +106,7 @@ public:
 		}
 	}
 
-	vector<basic_string_view<CharT, Traits> > splitsv(const  basic_string_view<CharT, Traits> &Separator) const
+	vector<basic_string_view<CharT, Traits> > splitsv(const basic_string_view<CharT, Traits> &Separator) const
 	{
 		vector<basic_string_view<CharT, Traits> > Result;
 		typename basic_string_view<CharT, Traits>::size_type p, start = 0;
@@ -89,6 +117,46 @@ public:
 			{
 				Result.emplace_back(basic_string_view<CharT, Traits>::substr(start, basic_string_view<CharT, Traits>::npos));
 				return Result;
+			}
+			else
+			{
+				Result.emplace_back(basic_string_view<CharT, Traits>::substr(start, p - start));
+				start = p + Separator.length();
+			}
+		}
+	}
+	template <class F>
+	void splitf(const basic_string_view<CharT, Traits> &Separator,F functor) const
+	{
+		typename basic_string_view<CharT, Traits>::size_type p, start = 0;
+		while (1)
+		{
+			p = basic_string_view<CharT, Traits>::find(Separator, start);
+			if (p == basic_string_view<CharT, Traits>::npos)
+			{
+				functor(basic_string_view<CharT, Traits>::substr(start, basic_string_view<CharT, Traits>::npos));
+				return ;
+			}
+			else
+			{
+				functor(basic_string_view<CharT, Traits>::substr(start, p - start));
+				start = p + Separator.length();
+			}
+		}
+
+	}
+	template <class T>
+	void splitc(const basic_string_view<CharT, Traits> &Separator,T &Result) const
+	{		
+		typename basic_string_view<CharT, Traits>::size_type p, start = 0;
+		Result.clear();
+		while (1)
+		{
+			p = basic_string_view<CharT, Traits>::find(Separator, start);
+			if (p == basic_string_view<CharT, Traits>::npos)
+			{
+				Result.emplace_back(basic_string_view<CharT, Traits>::substr(start, basic_string_view<CharT, Traits>::npos));
+				return ;
 			}
 			else
 			{
@@ -116,6 +184,7 @@ int main()
 	for (auto s : vector2)
 		cout << s << endl;
 
+	cout << "---- test splitsv -----" << endl;
 	vector<string_view> vector3 = strsv.splitsv(" "); // string_view => vector<string_view>
 	for (auto s : vector3)
 		cout << s << endl;
@@ -123,6 +192,26 @@ int main()
 	vector<string_view> vector4 = str.splitsv(" "); // string => vector<string_view>
 	for (auto s : vector4)
 		cout << s << endl;
+	cout << "---- test splitf -----" << endl;
+	strsv.splitf(" ", [&](const string_view &s)
+	{
+		cout << s <<"   ,Pos="<<(s.data() -strsv.data())<<" ,Len="<<s.length()<< endl;
+	}); // string_view => functor
+	cout << "---- test splitc -----" << endl;
+	vector<string_view> vector5;
+	strsv.splitc(" ", vector5); // splitc in vector<string_view>
+	for (auto s : vector5)
+		cout << s << endl; 
+	vector<string> vector6;
+	strsv.splitc(" ", vector6); // splitc in vector<string>
+	for (auto s : vector6)
+		cout << s << endl;
+
+	cout << "Join of string vector=" << string17::join(vector6, "_") << endl;
+	cout << "Join of string_view vector=" << string17::join(vector5, "_") << endl;
+	//cout << "pythonic Join of string =" << "-"s.join(vector6) << endl;
+	cout << "pythonic Join of string =" << string17("-").join(vector6) << endl;
+
 
     cout << "---- End of tests ----" << endl;
 
